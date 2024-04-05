5a7eadb1720417be6ad814a5f695174fd0fc7b6b
woodemai <n.savchenkoo73@gmail.com>
2024-04-06 01:45:05 +0300

    adding course page

--- src/entities/course/request/index.ts
+++ src/entities/course/request/index.ts
@@ -1,6 +1,13 @@
 import { CreateCourseRequest } from "./CreateCourseRequest";
 import { GetCoursesRequest } from "./GetCoursesRequest";
 import { EditCourseRequest } from "./EditCourseRequest";
-import { SubscribeStudentRequest } from "./SubcribeStudentRequest";
+import { SubscribeStudentRequest } from "./SubscribeStudentRequest";
+import { GetCourseRequest } from "./GetCourseRequest";
 
-export type { CreateCourseRequest, GetCoursesRequest, EditCourseRequest, SubscribeStudentRequest };
+export type {
+    CreateCourseRequest,
+    GetCoursesRequest,
+    GetCourseRequest,
+    EditCourseRequest,
+    SubscribeStudentRequest,
+};
--- src/features/course/CourseList.tsx
+++ src/features/course/CourseList.tsx
@@ -10,10 +10,10 @@ const CourseList: FC<Props> = ({ courses }) => {
 
     return (
         <ul className="flex flex-col gap-y-4 w-full h-full">
-            {courses?.map(course => <li key={course.id}><CourseCard description={course.description} title={course.title} /></li>)}
+            {courses?.map(course => <li key={course.id}><CourseCard id={course.id} description={course.description} title={course.title} /></li>)}
             {!courses?.length && <div className="flex justify-center items-center w-full h-full">
                 <h2>Курсы не найдены</h2>
-                </div>}
+            </div>}
         </ul>
     )
 }
